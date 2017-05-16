7fbad0572c4fc02cf9f18e4bc728152b7e6df8cb
Nora Sandler <nlsandler@gmail.com>
2017-05-15 21:17:43 -0700

    punctuation clean up

--- test_util.py
+++ test_util.py
@@ -22,6 +22,50 @@ class UtilTests(unittest.TestCase):
     def test_get_syllable_count(self):
         word = "Syllable."
         self.assertEqual(3, util.get_syllable_count(word))
+    def test_strip_punctuation(self):
+        """Strip out unclosed quotation mark"""
+        text = "\"Quote"
+        self.assertEqual("Quote", util.strip_straight_quotes(text))
+    def test_strip_balanced_punctuation(self):
+        """Don't modify balanced quotation marks"""
+        text = "\"Quote\""
+        self.assertEqual(text, util.strip_straight_quotes(text))
+    def test_strip_third_quote(self):
+        """Remove extra, unclosed quote, but not first closed quotes"""
+        text = "She said \"foo.\" Then he said \"Bar "
+        self.assertEqual("She said \"foo.\" Then he said Bar ",
+            util.strip_straight_quotes(text))
+    def test_strip_parentheses(self):
+        """Remove unclosed parenthesis"""
+        text = "(Parentheses"
+        self.assertEqual("Parentheses", util.strip_unbalanced_punctuation(text, "(", ")"))
+    def test_strip_backwards_parentheses(self):
+        """Remove parentheses that are in wrong order"""
+        text = ")Paren("
+        self.assertEqual("Paren", util.strip_unbalanced_punctuation(text, "(", ")"))
+    def test_strip_backwards_quotes(self):
+        """Remove quotes that are in wrong order,
+        using punctuation to identify opening and closing quotes"""
+        text = ".\" She said, \""
+        self.assertEqual(". She said, ", util.strip_straight_quotes(text))
+    def test_strip_interleaved_quotes(self):
+        """Correctly handle balanced quotes mixed with unbalanced ones."""
+        text = "yes,\" he said. \"Yes, but \"no.\""
+        self.assertEqual("yes, he said. Yes, but \"no.\"",
+            util.strip_straight_quotes(text))
+    def test_strip_interleaved_parentheses(self):
+        """Correctly handle closed parentheses mixed with unclosed ones."""
+        text = "(It's true (but (it) might not be)"
+        self.assertEqual("It's true (but (it) might not be)",
+            util.strip_unbalanced_punctuation(text,"(", ")"))
+    def test_curly_quotes(self):
+        """Correctly strip curly quotes"""
+        text = "“foo”"
+        self.assertEqual("“foo”", util.strip_unbalanced_punctuation(text, "“", "”"))
+    def test_unbalanced_curly_quotes(self):
+        text = "”foo”"
+        self.assertEqual("foo", util.strip_unbalanced_punctuation(text, "“", "”"))
+
 
 def main():
     unittest.main()
--- util.py
+++ util.py
@@ -22,3 +22,62 @@ def get_syllable_count(word):
     word = normalize(word)
     phones = pronouncing.phones_for_word(word)[0]
     return len(pronouncing.stresses(phones))
+
+def strip_unbalanced_punctuation(text, opening_char, closing_char):
+    """Remove unbalanced punctuation (e.g parentheses) from text"""
+    #iterate over text
+    #when you encounter an opening_char, start looking for a closing closing_char
+    #if you don't find one by the end of the text, remove opening_char
+    #when you encounter a closing_char if it doesn't match an opening_char, remove it
+    #so we need to track how many opening parens we have, and where they are
+    opening_chars = [] #opening chars we're looking for a match for
+    unmatched_closing_chars = [] #closing chars that don't have a match - delete at end of loop
+    for idx, c in enumerate(text):
+        if c == opening_char:
+            opening_chars.append(idx)
+        elif c == closing_char:
+            if opening_chars:
+                opening_chars.pop()
+            else:
+                unmatched_closing_chars.append(idx)
+    char_indices = [i for (i,_) in enumerate(text) if not(i in opening_chars or i in unmatched_closing_chars)]
+    stripped_text = "".join([text[i] for i in char_indices])
+    return stripped_text
+
+def strip_punct_lambda(text, delim, is_opening):
+    """Remove unbalanced punctuation (e.g parentheses) from text"""
+    #iterate over text
+    #when you encounter an opening_char, start looking for a closing closing_char
+    #if you don't find one by the end of the text, remove opening_char
+    #when you encounter a closing_char if it doesn't match an opening_char, remove it
+    #so we need to track how many opening parens we have, and where they are
+    opening_chars = [] #opening chars we're looking for a match for
+    unmatched_closing_chars = [] #closing chars that don't have a match - delete at end of loop
+    for idx, c in enumerate(text):
+        if c == delim:
+            if is_opening(text, idx):
+                opening_chars.append(idx)
+            else:
+                if opening_chars:
+                    opening_chars.pop()
+                else:
+                    unmatched_closing_chars.append(idx)
+    char_indices = [i for (i,_) in enumerate(text) if not(i in opening_chars or i in unmatched_closing_chars)]
+    stripped_text = "".join([text[i] for i in char_indices])
+    return stripped_text
+
+def strip_straight_quotes(text):
+    def is_opening(text, i):
+        if text[i-1] in string.whitespace:
+            # Bar. "Foo...
+            return True
+        elif len(text) == i + 1:
+            #It's the last quote in the text so assume it's closing
+            return False
+        elif text[i+1] in string.whitespace:
+            # foo." Bar...
+            return False
+        else:
+            #No clear indication, just guess True
+            return True
+    return strip_punct_lambda(text, "\"", is_opening)
